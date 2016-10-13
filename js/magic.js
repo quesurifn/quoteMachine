8ba28f0ea0031ecd54cf4f6cc3809378d430d330
Kyle <kyle.c.r.fahey@gmail.com>
2016-10-12 20:09:35 -0500

    Fix Twitter function - now dispaying right text

--- js/magic.js
+++ js/magic.js
@@ -1,10 +1,14 @@
 
 $(document).ready(function() {
+
 var colors = ['#E9967A', '#FF8C00', '#4682B4', '#CD853F', '#008080', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#fb9664', '#bdbb99', '#77b1a9', '#73a857'];
+var currentQuote;
+var currentAuthor;
+
 var quoteGen = function() {
 $("#author").empty();
 $("#quote").empty();
-
+console.log("err");
 $.ajax({
    type : "GET",
    url : "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
@@ -14,14 +18,21 @@ $.ajax({
       $("#quote").append(data.quote);
       console.log("appended")
       $("#author").append(data.author);
-
+      currentQuote = data.quote;
+      currentAuthor = data.author;
+      $("#tweet").attr('href','https://twitter.com/intent/tweet?hashtags=quotes&text='
+       + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
    },
    error : function(data) {
      console.log("error");
    }
  });
-
  };
+
+quoteGen();
+
+
+
 
 
     $("#getQuote").click(function(){
