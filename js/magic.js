76c5a673bf2673c1e4797b470d49e432b5cad986
Kyle <kyle.c.r.fahey@gmail.com>
2016-10-12 19:30:35 -0500

    Fix Quotes now display

--- js/magic.js
+++ js/magic.js
@@ -1,13 +1,28 @@
 
 $(document).ready(function() {
 var colors = ['#E9967A', '#FF8C00', '#4682B4', '#CD853F', '#008080', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#fb9664', '#bdbb99', '#77b1a9', '#73a857'];
+var quoteGen = function() {
+$("#author").empty();
+$("#quote").empty();
 
+$.ajax({
+   type : "GET",
+   url : "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
+   dataType: "json",
+   beforeSend: function(xhr){xhr.setRequestHeader("X-Mashape-Authorization", "5ekS2kpDYzmshmMVxnjCTWJAbAxcp12eSCLjsnCic4npUWTOyN");},
+   success : function(data) {
+      $("#quote").append(data.quote);
+      console.log("appended")
+      $("#author").append(data.author);
 
-    $("#getQuote").on("click", function(){
-      $.getJSON("http://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
-        $(".id").html(JSON.stringify(json));
-      });
-    });
+   },
+   error : function(data) {
+     console.log("error");
+   }
+ });
+
+ };
+
 
     $("#getQuote").click(function(){
       var color = Math.floor(Math.random() * colors.length);
@@ -17,5 +32,6 @@ var colors = ['#E9967A', '#FF8C00', '#4682B4', '#CD853F', '#008080', '#27ae60', 
       $(".btn").animate({
         backgroundColor: colors[color]
       }, 1000);
+      quoteGen();
     });
-  });
+    });
